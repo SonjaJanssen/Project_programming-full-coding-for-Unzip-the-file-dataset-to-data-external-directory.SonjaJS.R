60f9e30d667da6d974b6b573f8fc0dbef6644de9
Sonja Sahebzad <123073089+SonjaJanssen@users.noreply.github.com>
2023-07-05 23:48:16 +0200

    Update Assignment programming full coding for Unzip the file dataset to data external directory.SonjaJS.R

--- Assignment programming full coding for Unzip the file dataset to data external directory.SonjaJS.R
+++ Assignment programming full coding for Unzip the file dataset to data external directory.SonjaJS.R
@@ -1,7 +1,7 @@
 ## Assignment programming full coding for Unzip the file dataset to data external directory
 #   Lectures: By Professor Jeffrey Leek
 #   University: John Hopkins University Bloomberg School of Public Health
-#   Codes: @SonjaJS
+#   Codes: @Sonja Janssen-Sahebzad
 #   Notes: Created with R 4.2.2 for Windows
 #   Date:  30 March 2023
 
